5a06c61a3497878b84b3e52c4513cea7629c9619
Himanshu Khairajani <46777429+Khairajani@users.noreply.github.com>
2025-01-22 09:09:28 +0000

    database name corrected

--- bd5/lib/base.js
+++ bd5/lib/base.js
@@ -2,7 +2,7 @@ import { DataTypes, Sequelize } from "sequelize";
 
 let sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./databse.sqlite",
+  storage: "./database.sqlite",
 });
 
 export { DataTypes, sequelize };
